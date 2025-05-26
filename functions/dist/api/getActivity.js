5e8563511c069c92cb8b245cc34038ebe7f6cec2
Theo <theo@theo.dev.dev>
2025-05-26 10:45:33 +0200

    rm dist

--- functions/dist/api/getActivity.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getActivity = getActivity;
-async function getActivity(req, res) {
-    res.status(404).send('getActivity');
-}
--- functions/dist/api/getSelf.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getSelf = getSelf;
-const jwt_1 = require("../jwt");
-async function getSelf(req, res) {
-    const user = await (0, jwt_1.extractUserFromJwt)(req);
-    res.status(200).json({ user });
-}
